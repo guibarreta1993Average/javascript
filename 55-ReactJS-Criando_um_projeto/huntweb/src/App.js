352b66ff59c9d98bbffc1db8f690070365c79745
Barreta <guilherme.barreta@average.com.br>
2019-10-03 17:50:27 -0300

    penultima aula

--- 55-ReactJS-Criando_um_projeto/huntweb/src/App.js
+++ 55-ReactJS-Criando_um_projeto/huntweb/src/App.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
+import Routes from './routes'
+
+import "./style.css"
 import Header from './components/Header'
+import Main from './pages/main'
 
 const App = () =>(
       <div className="App">
         <Header/>
+        <Routes/>
       </div>
     )
 
---
+++ 55-ReactJS-Criando_um_projeto/huntweb/src/pages/product/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react'
+export default class Product extends Component{
+    render (){
+        return <h1>Product</h1>
+    }
+}
